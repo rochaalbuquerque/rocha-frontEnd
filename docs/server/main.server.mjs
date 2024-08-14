c7ef8bc8691d551ea2af1bd7ebb41a1ab2631c11
Alex Rocha <alexrochadealbuquerque@gmail.com>
2024-08-14 15:54:29 -0300

    [main] update - build e index.html

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XGWPGHDA.mjs";import"./chunk-XLC7SIIK.mjs";import"./chunk-BAZY6RZB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MAZJFAI4.mjs";import"./chunk-LTD46IKC.mjs";import"./chunk-VAOZ26CB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
