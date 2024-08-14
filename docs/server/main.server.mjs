5cfc79c3b755a2c0ac4baa3576268b51848f3319
Alex Rocha <alexrochadealbuquerque@gmail.com>
2024-08-14 16:31:05 -0300

    [gh-pages] update - build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MAZJFAI4.mjs";import"./chunk-LTD46IKC.mjs";import"./chunk-VAOZ26CB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4T7BEDAI.mjs";import"./chunk-LTD46IKC.mjs";import"./chunk-VAOZ26CB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
